5b24c0d2eee7bb7223804f44c07a55708fdaeae1
Meaghan <meaghan.vella@gmail.com>
2017-11-05 11:12:49 -0500

    changed structure

--- src/model/Command.java
+++
@@ -1,7 +0,0 @@
-package model;
-import java.util.Iterator;
-
-
-public interface Command {
-	public void execute(Iterator args);
-}
--- src/model/DBConstants.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public class DBConstants {
-	public static final String DB_DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";
-	public static final String DB_CrsSystem = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};DBQ=loginfunctionality.mdb";
-}
